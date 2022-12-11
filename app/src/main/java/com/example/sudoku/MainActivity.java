83240e0587f5e6700ace16fa966f1d096d6aff9e
HamHyeonJun <hyunjun003@naver.com>
2022-12-12 00:47:24 +0900

    Conflict Done

--- app/src/main/java/com/example/sudoku/MainActivity.java
+++ app/src/main/java/com/example/sudoku/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.app.Dialog;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -12,16 +14,17 @@ import android.widget.TableRow;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.material.shape.CutCornerTreatment;
 import com.google.android.material.tabs.TabLayout;
 
 import java.time.temporal.ChronoUnit;
 
 public class MainActivity extends AppCompatActivity {
-    TableLayout table ,numberPad;
+    TableLayout table, numberPad;
 
     CustomButton[][] buttons = new CustomButton[9][9];
     BoardGenerator board = new BoardGenerator();
-    Button numP1,numP2,numP3,numP4,numP5,numP6,numP7,numP8,numP9,cancel_button,delete_button;
+    Button numP1, numP2, numP3, numP4, numP5, numP6, numP7, numP8, numP9, cancel_button, delete_button;
     CustomButton selectedB;
     Dialog dialog;
 
@@ -30,12 +33,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         table = (TableLayout) findViewById(R.id.tableLayout);
-        numberPad =(TableLayout) findViewById(R.id.numberPad);
+        numberPad = (TableLayout) findViewById(R.id.numberPad);
         makeBoard();
         insertNumPad();
     }
 
-    public void insertNumPad(){
+    public void insertNumPad() {
         numP1 = findViewById(R.id.numP1);
         numP1.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,15 +116,16 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View view) {
                 selectedB.reset();
                 numberPad.setVisibility(View.INVISIBLE);
+                selectedB.setBackgroundColor(Color.WHITE);
             }
         });
     }
 
-// numPad에서 숫자 클릭시 빈칸에 넣어주는 부분
-    public void onClickEvent(View view, int a){
+    // numPad에서 숫자 클릭시 빈칸에 넣어주는 부분
+    public void onClickEvent(View view, int a) {
         selectedB.insertNum(a);
         numberPad.setVisibility(View.INVISIBLE);
-        conflictTest(a);
+        setColor(conflictTest(a));
     }
 
     //gameBoard 생성
@@ -145,12 +149,12 @@ public class MainActivity extends AppCompatActivity {
             for (int j = 0; j < 9; j++) {
                 int finalI = i;
                 int finalJ = j;
-                layoutParams.setMargins(5, 5, 5,5);
+                layoutParams.setMargins(5, 5, 5, 5);
                 buttons[i][j] = new CustomButton(this, i, j);
                 buttons[i][j].setLayoutParams(layoutParams);
 // 70% 채우기 시작
                 int randomInt = (int) (Math.random() * 10);
-                if (randomInt>3) {
+                if (randomInt > 3) {
                     buttons[i][j].set(board.get(i, j));
                     buttons[i][j].setClickable(false);
                 } else {
@@ -172,48 +176,60 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void conflictTest(int testNum){
+
+    public int conflictTest(int testNum) {
         int selectRow = selectedB.row;
         int selectCol = selectedB.col;
-
-
-        for(int c=0;c<9; c++){
+        CustomButton NumberCustom;//row
+        CustomButton NumberCustom2;//col
+        CustomButton NumberCustomMe = buttons[selectRow][selectCol];;
+        CustomButton NumberCustom3; //3x3
+        for (int c = 0; c < 9; c++) {//row col 검사
             //row에 같은값 있는지 검사
-            int exNum = board.get(selectRow, c);
-            System.out.println("c is "+c+"\nexNum is "+exNum);
-
-            if (testNum == exNum){
-                setConflict();
-
+            NumberCustom = buttons[selectRow][c];
+            NumberCustom2 = buttons[c][selectCol];
+            if (testNum == NumberCustom.get() && c != selectCol) {//row 값을 검사함 ( 같은 숫자가 있는지 확인 )
+//                setConflict(NumberCustomMe);
+                System.out.println("Row");
+                return 1;
             }
-            else{
-                unsetConflict();
-
+            else if (testNum == NumberCustom2.get() && c != selectRow) {//col 값을 검사함 ( 같은 숫자가 있는지 확인 )
+//                setConflict(NumberCustomMe);
+                System.out.println("Col");
+                return 1;
             }
-            //col에 같은값 있는지 검사
-//            int exNum2 = buttons[c][selectCol].get();
-//            if (testNum == exNum2){
-//                setConflict();
-//            }
-//            else{
-//                unsetConflict();
-//            }
         }
-
-
-
-
-
-
-        //row col에 맞춰서 맞는 group 반환하는 함수 호출
-        //반환받은 group값을 통해서 검사
-
+        int tmpRow = selectRow/3*3;
+        int tmpCol = selectCol/3*3;
+        for (int r =tmpRow; r<=tmpRow+2; r++){
+            for (int c=tmpCol; c<=tmpCol+2; c++){
+                NumberCustom3=buttons[r][c];
+                if (testNum == NumberCustom3.get() && r!=selectRow && c !=selectCol){
+//                    setConflict(NumberCustomMe);
+                    System.out.println("3x3");
+                    return 1;
+                }
+            }
+        }
+        return 0;
     }
 
-    public void setConflict(){
-        Toast.makeText(this, "Conflict!", Toast.LENGTH_SHORT).show();
+    public void setColor(int status){
+        System.out.println(status);
+        if(status==1){
+            setConflict();
+        }
+        else{
+            unsetConflict();
+        }
     }
-    public void unsetConflict(){
-        Toast.makeText(this, "Ok!", Toast.LENGTH_SHORT).show();
+
+
+    public void setConflict() {
+        selectedB.setBackgroundColor(Color.RED);
+    }
+
+    public void unsetConflict() {
+        selectedB.setBackgroundColor(Color.WHITE);
     }
 }
